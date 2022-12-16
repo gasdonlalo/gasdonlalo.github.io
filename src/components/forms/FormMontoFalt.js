5a54ee3778cda2cbf73157412400c5549bd843dd
franc <blanco.email>
2022-12-15 18:42:52 -0600

    añadido recolector de datos y funcion post en monto faltante

--- src/components/forms/FormMontoFalt.js
+++ src/components/forms/FormMontoFalt.js
@@ -1,7 +1,19 @@
-function FormMontoFalt() {
+function FormMontoFalt({ handle, enviar }) {
   return (
     <div className="container">
-      <form onSubmit="">
+      <form onSubmit={enviar}>
+        <div className="mb-3">
+          <label for="exampleInputEmail1" className="form-label">
+            ID de empleado
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            aria-describedby="emailHelp"
+            name="idempleado"
+            onChange={handle}
+          />
+        </div>
         <div className="mb-3">
           <label for="exampleInputEmail1" className="form-label">
             Nombre
@@ -10,8 +22,9 @@ function FormMontoFalt() {
             type="text"
             className="form-control"
             aria-describedby="emailHelp"
-            name="banco"
-            onChange=""
+            defaultvalue="ola"
+            disabled
+            readonly
           />
         </div>
         <div className="mb-3">
@@ -20,10 +33,11 @@ function FormMontoFalt() {
           </label>
           <input
             type="number"
+            step="0.01"
             className="form-control"
             aria-describedby="emailHelp"
-            name="clabe"
-            onChange=""
+            name="monto"
+            onChange={handle}
           />
         </div>
 
--- src/components/pages/MontoFaltante.js
+++ src/components/pages/MontoFaltante.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import FormMontoFalt from "../../components/forms/FormMontoFalt";
+import Axios from "../../Caxios/Axios";
 function MontoFaltante() {
+  const [data, setData] = useState([]);
+  //recibe los datos del formulario
+  const handle = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data);
+  };
+  //añadir ruta
+  const enviarDatos = async () => {
+    const resp = await Axios.post("/", data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+    console.log(resp);
+  };
+
+  const enviar = () => {
+    enviarDatos();
+  };
   return (
     <div className="ContentMain">
       <div className="Main">
@@ -9,7 +28,7 @@ function MontoFaltante() {
             Volver al despacho
           </Link>
           <h4>Insertar Montos faltantes</h4>
-          <FormMontoFalt />
+          <FormMontoFalt handle={handle} enviar={enviar} />
         </div>
       </div>
     </div>
