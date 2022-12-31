dc41b4323bc5983ee08425fc4867d335cee592c6
Blanco <blanco.email>
2022-12-31 16:47:05 -0600

    cambios de estilo en home, arreglado nombre de clases

--- src/GUI/Layout.js
+++ src/GUI/Layout.js
@@ -4,16 +4,16 @@ function Layout({ children }) {
   return (
     <div className="flex_container">
       <div>
-      <Header />
-      <div className="section Content">{children}</div>
+        <Header />
+        <div className="section Content">{children}</div>
       </div>
 
       {/* Pie de pagina */}
       <div>
-        <div class="">
-          <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-10 border-top">
-            <div class="col-md-4 d-flex align-items-center">
-              <span class="mb-3 mb-md-0 text-muted">
+        <div className="container-fluid">
+          <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-10 border-top">
+            <div className="col-md-4 d-flex align-items-center">
+              <span className="mb-3 mb-md-0 text-muted">
                 &copy; Gasolinera Don Lalo
               </span>
             </div>
@@ -21,7 +21,6 @@ function Layout({ children }) {
         </div>
       </div>
     </div>
-    
   );
 }
 export default Layout;
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,66 +1,86 @@
-import { Fragment } from "react";
 import { Link } from "react-router-dom";
-import tablas from "../../../App.css";
 
 function Home() {
   return (
-    <div className="d-flex justify-content-evenly flex-wrap m-1" style={{alignContent: "stretch"}}>
+    <div className="home">
       <div className="card text-center" style={{ width: "18rem" }}>
-        <Link to="/despacho"><i className="bi bi-ev-station" style={{ fontSize: "72pt" }}></i></Link>
+        <Link to="/despacho">
+          <i className="bi bi-ev-station" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Despacho</h5>
         </div>
       </div>
 
       <div className="card text-center" style={{ width: "18rem" }}>
-        <Link to="/"><i class="bi bi-tools" style={{ fontSize: "72pt" }}></i></Link>
+        <Link to="/">
+          <i className="bi bi-tools" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Mantenimiento</h5>
         </div>
       </div>
 
       <div className="card text-center" style={{ width: "18rem" }}>
-        <Link tp="/"><i class="bi bi-box2" style={{ fontSize: "72pt" }}></i></Link>
+        <Link tp="/">
+          <i className="bi bi-box2" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Almacen</h5>
         </div>
       </div>
 
-      <div className="card text-center" style={{ width: "18rem" }} to="/calidad">
-        <Link to="/calidad"><i class="bi bi-patch-check" style={{ fontSize: "72pt" }}></i></Link>
+      <div
+        className="card text-center"
+        style={{ width: "18rem" }}
+        to="/calidad"
+      >
+        <Link to="/calidad">
+          <i className="bi bi-patch-check" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Calidad</h5>
         </div>
       </div>
 
-      <div className="card text-center" style={{ width:"18rem" }}>
-        <Link to="/"><i class="bi bi-people" style={{fontSize:"72pt"}}></i></Link>
+      <div className="card text-center" style={{ width: "18rem" }}>
+        <Link to="/">
+          <i className="bi bi-people" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Recursos Humanos</h5>
         </div>
       </div>
 
       <div className="card text-center shadow-sm" style={{ width: "18rem" }}>
-        <Link to="/"><i class="bi bi-card-checklist" style={{fontSize:"72pt"}}></i></Link>
+        <Link to="/">
+          <i className="bi bi-card-checklist" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Administrativo</h5>
         </div>
       </div>
 
       <div className="card text-center" style={{ width: "18rem" }}>
-        <Link to="/"><i class="bi bi-shield-lock" style={{ fontSize: "72pt" }}></i></Link>
+        <Link to="/">
+          <i className="bi bi-shield-lock" style={{ fontSize: "72pt" }}></i>
+        </Link>
         <div className="card-body">
           <h5>Seguridad</h5>
         </div>
       </div>
 
       <div className="card text-center" style={{ width: "18rem" }}>
-        <Link to="/"><i class="bi bi-file-earmark-pdf" style={{fontSize:"72pt"}}></i></Link>
+        <Link to="/">
+          <i
+            className="bi bi-file-earmark-pdf"
+            style={{ fontSize: "72pt" }}
+          ></i>
+        </Link>
         <div className="card-body">
           <h5>Documentos SGC Don Lalo</h5>
         </div>
       </div>
-
     </div>
   );
 }
