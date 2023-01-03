e6979f7dfc3661d796f5839427c1bb86d1f37ab3
Blanco <blanco.email>
2023-01-03 10:14:27 -0600

    ruta captura añadida

---
+++ src/components/pages/RecursosHumanos/SolicitudesEmpleo.js
@@ -0,0 +1,5 @@
+function SolicitudesEmpleo() {
+  return <div>SolicitudesEmpleo</div>;
+}
+
+export default SolicitudesEmpleo;
--- src/routes/Rutas.js
+++ src/routes/Rutas.js
@@ -19,6 +19,8 @@ import GEU from "../components/pages/despacho/reporteria/GraficaEvUnifome";
 import DRE from "../components/pages/despacho/reporteria/GraficaRecolEfectivo";
 import GPD from "../components/pages/despacho/reporteria/GraficaPasoDes";
 import GRD from "../components/pages/despacho/reporteria/GraficaRecursosDes";
+//importaciones recursos humanos
+import SolicitudesEmpleo from "../components/pages/RecursosHumanos/SolicitudesEmpleo";
 
 function Rutas() {
   return (
@@ -48,6 +50,10 @@ function Rutas() {
           <Route path="salida-no-conforme" element={<SalidaNoConforme />} />
           <Route path="salida-no-conforme-files" element={<SNR />} />;
           <Route path="*" element={<Notfound />} />
+        </Route>
+
+        <Route path="/recursos-humanos" element={<Layout />}>
+          <Route path="captura-solicitud" element={<SolicitudesEmpleo />} />
         </Route>
       </Routes>
       {/* <Routes>
