90c9a4b2f7eec0f1657bd1eaaaa9493c27b400bc
jair Nuñez <halodj_13@hotmail.com>
2023-01-09 09:18:48 -0600

    el pepe

--- src/components/forms/FormDocumentos.js
+++ src/components/forms/FormDocumentos.js
@@ -1,6 +1,38 @@
-function FormDocumentos() {
+function FormDocumentos({handle , datos, enviar}) {
   return (
-    <div></div>
+    <div className="container">
+      <form onSubmit={enviar}>
+        <div className="row">
+          <div className="mb-3 col-6">
+            <label>empleado</label>
+            <select
+              className="form-control"
+              name="idempleado"
+              onChange={handle}
+            >
+            <option value="0"> Selecciona un empleado </option>
+            {!datos.data
+              ? false
+              : datos.data.response.map((e) => {
+                return (
+                  <option
+                  value={e.idempleado}
+                  >{`${e.nombre} ${e.apellido_paterno} ${e.apellido_materno}`}</option>
+                  );
+                })}
+            </select>
+          </div>
+        </div>
+        <table className="table align-middle">
+          <thead>
+            <tr>
+              <th scope="col">Documentos</th>
+            </tr>
+          </thead>
+
+        </table>
+      </form>
+    </div>
   );
 };
 
--- src/components/pages/RecursosHumanos/Documentos.js
+++ src/components/pages/RecursosHumanos/Documentos.js
@@ -1,12 +1,45 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import Axios from "../../../Caxios/Axios";
+import useGetData from "../../../hooks/useGetData";
+import FormDocumentos from "../../forms/FormDocumentos";
 
 function Documentos() {
+
+  const [data, setData] = useState([]);
+
+  const handle = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+    console.log(data);
+  };
+
+  //para poder obtener todos los empleados de todos los departamentos
+  const empleados = useGetData("/empleado?departamento");
+
+  const enviar = (e) => {
+    e.preventDefault();
+    enviarDatos();
+    console.log(data);
+  }
+
+  const enviarDatos = async () => {
+    const resp = await Axios.post("/control-documento", data);
+    console.log(resp);
+    if (resp.status === 200) {
+      window.alert("correcto");
+    } else {
+      window.alert("incorrecto");
+    }
+    console.log(resp)
+  }
+
   return (
     <div className="Main">
       <div>
         <Link className="Link-primary" to="/recursos-humanos">Volver a recursos humanos</Link>
       </div>
-        <h4 className="border-bottom">Documentos de trabajo</h4>
+      <h4 className="border-bottom">Documentos de trabajo</h4>
+      <FormDocumentos enviar={enviar} datos={empleados } />
     </div>
   )
 }
