a3e7b6dace10449cf7ab0e6a341188f026d75a17
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2023-01-08 22:28:49 -0600

    grafica captura entrada

---
+++ src/GUI/HeaderComponents.js
@@ -0,0 +1,29 @@
+import { Link } from "react-router-dom";
+
+const HeaderComponents = ({ urlBack, textUrlback, title, children }) => {
+  return (
+    <div className="Main">
+      <div>
+        <div className="d-flex justify-content-between border-bottom align-items-center">
+          <div>
+            <Link className="Link-primary" to={urlBack}>
+              {textUrlback}
+            </Link>
+
+            <h4>{title}</h4>
+          </div>
+          <div>{children}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+HeaderComponents.defaultProps = {
+  urlBack: "/",
+  textUrlback: "Regresar al inicio",
+  title: "Titulo por defecto",
+  children: false,
+};
+
+export default HeaderComponents;
