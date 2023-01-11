b1b76ffceffc3f5cf22af3c4dcaedb86c36ea584
Blanco <blanco.email>
2023-01-11 09:45:41 -0600

    optimizado consulta octanoso

--- src/Caxios/Axios.js
+++ src/Caxios/Axios.js
@@ -1,5 +1,5 @@
 import Axios from "axios";
 export default Axios.create({
-  //baseURL: "http://192.168.137.1:4000/api",
-  baseURL: "http://192.168.0.103:4000/api",
+  baseURL: "http://192.168.56.1:4000/api",
+  //baseURL: "http://192.168.0.103:4000/api",
 });
