ecc3e0dce3a8ef6fdf675f4e1e80bd8ef5cad9fc
Jose Luis Cabrera Aguirre <aj3amdua@gmail.com>
2023-02-04 19:03:39 -0600

    permisos usuarios

--- src/GUI/Auth.js
+++ src/GUI/Auth.js
@@ -3,9 +3,11 @@ import Axios from "../Caxios/Axios";
 import HeaderForm from "./HeaderForm";
 import Loader from "../components/assets/Loader";
 
-const Auth = ({ setAuth }) => {
+const Auth = () => {
   const [success, setSuccess] = useState({ status: null, pending: false });
   const [msgError, setMsgError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+
   const auth = async (e) => {
     e.preventDefault();
     setSuccess({ ...success, pending: true });
@@ -30,8 +32,13 @@ const Auth = ({ setAuth }) => {
       }, 2000);
     }
   };
+
+  const tooglePassword = () => {
+    setShowPassword(!showPassword);
+  };
+
   return (
-    <div style={{ width: "100vw", height: "100vh" }} className="d-flex">
+    <div className="d-flex align-items-center justify-content-center vh-100 vw-100">
       <div className="m-auto">
         <form className="shadow p-3 rounded" onSubmit={auth}>
           <HeaderForm title="Inicia Sesión" />
@@ -48,13 +55,28 @@ const Auth = ({ setAuth }) => {
             </div>
             <div className="col-12">
               <label className="form-label mb-0">Contraseña</label>
-              <input
-                type="password"
-                placeholder="password"
-                className="form-control"
-                name="password"
-                required
-              />
+              <div className="input-group">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  placeholder="password"
+                  className="form-control"
+                  name="password"
+                  required
+                />
+                <button
+                  className="btn btn-outline-dark"
+                  type="button"
+                  onClick={tooglePassword}
+                >
+                  <i
+                    className={
+                      !showPassword
+                        ? "fa-regular fa-eye"
+                        : "fa-regular fa-eye-slash"
+                    }
+                  />
+                </button>
+              </div>
             </div>
           </div>
           <div className="mt-2">
